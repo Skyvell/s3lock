9e5dfb76acfea93ddf78595b0f2bfb6d88f63fed
Ted Skyvell <tskyvell@gmail.com>
2023-08-27 19:19:07 +0200

    Updated tests to work with new version.

--- s3lock/s3lock_test.go
+++ s3lock/s3lock_test.go
@@ -17,7 +17,7 @@ func TestAcquireAndReleaseLock(t *testing.T) {
 	// Arrange.
 	ctx := context.Background()
 	config := setupConfig(ctx)
-	lock1 := NewS3Lock(config, "testlock", "versioningbucketcrossbreed", "key-39", 15)
+	lock1 := NewS3Lock(config, "testlock", "versioningbucketcrossbreed", "key-104", 15)
 
 	// Act - acquire lock.
 	err := lock1.AcquireLock(ctx)
@@ -26,11 +26,11 @@ func TestAcquireAndReleaseLock(t *testing.T) {
 	if err != nil {
 		t.Fatalf("AcquireLock failed: %v", err)
 	}
-	isOwner, err := lock1.isCurrentLockOwner(ctx)
+	err = lock1.syncLockState(ctx)
 	if err != nil {
-		t.Fatalf("isCurrentLockOwner failed: %v", err)
+		t.Fatalf("Failed to sync lock state: %s", err)
 	}
-	if !isOwner {
+	if lock1.lockState.state != LockAcquired {
 		t.Fatalf("%s is not the owner of this lock.", lock1.Uuid)
 	}
 
@@ -41,7 +41,7 @@ func TestAcquireAndReleaseLock(t *testing.T) {
 	}
 
 	// Assert - release lock.
-	versions, err := utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-39")
+	versions, err := utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-104")
 	if err != nil {
 		t.Fatalf("GetAllObjectVersions failed: %v", err)
 	}
@@ -54,8 +54,8 @@ func TestReleaseLockNotOwned(t *testing.T) {
 	// Arrange.
 	ctx := context.Background()
 	config := setupConfig(ctx)
-	lock1 := NewS3Lock(config, "testlock1", "versioningbucketcrossbreed", "key-39", 15)
-	lock2 := NewS3Lock(config, "testlock2", "versioningbucketcrossbreed", "key-39", 15)
+	lock1 := NewS3Lock(config, "testlock1", "versioningbucketcrossbreed", "key-101", 15)
+	lock2 := NewS3Lock(config, "testlock2", "versioningbucketcrossbreed", "key-101", 15)
 
 	// Act - acquire lock.
 	err := lock1.AcquireLock(ctx)
@@ -64,11 +64,11 @@ func TestReleaseLockNotOwned(t *testing.T) {
 	if err != nil {
 		t.Fatalf("AcquireLock failed: %v", err)
 	}
-	isOwner, err := lock1.isCurrentLockOwner(ctx)
+	err = lock1.syncLockState(ctx)
 	if err != nil {
-		t.Fatalf("isCurrentLockOwner failed: %v", err)
+		t.Fatalf("Failed to sync lock state: %s", err)
 	}
-	if !isOwner {
+	if lock1.lockState.state != LockAcquired {
 		t.Fatalf("%s is not the owner of this lock.", lock1.Uuid)
 	}
 
@@ -79,12 +79,16 @@ func TestReleaseLockNotOwned(t *testing.T) {
 	}
 
 	// Assert - release lock.
-	versions, err := utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-39")
+	err = lock2.syncLockState(ctx)
 	if err != nil {
-		t.Fatalf("GetAllObjectVersions failed: %v", err)
+		t.Fatalf("Failed to sync lock state: %s", err)
 	}
-	if len(versions) != 1 {
+
+	if len(lock2.lockState.entries) != 1 {
 		t.Fatalf("There should be 1 lock file.")
+	}
+	if lock2.lockState.state != LockOccupied {
+		t.Fatalf("Lock state should be %v", LockOccupied)
 	}
 }
 
@@ -92,8 +96,8 @@ func TestAquireLockAfterTimeout(t *testing.T) {
 	// Arrange.
 	ctx := context.Background()
 	config := setupConfig(ctx)
-	lock1 := NewS3Lock(config, "testlock1", "versioningbucketcrossbreed", "key-39", 4)
-	lock2 := NewS3Lock(config, "testlock2", "versioningbucketcrossbreed", "key-39", 4)
+	lock1 := NewS3Lock(config, "testlock1", "versioningbucketcrossbreed", "key-102", 4)
+	lock2 := NewS3Lock(config, "testlock2", "versioningbucketcrossbreed", "key-102", 4)
 
 	// Act - acquire lock.
 	err := lock1.AcquireLock(ctx)
@@ -102,11 +106,11 @@ func TestAquireLockAfterTimeout(t *testing.T) {
 	if err != nil {
 		t.Fatalf("AcquireLock failed: %v", err)
 	}
-	isOwner, err := lock1.isCurrentLockOwner(ctx)
+	err = lock1.syncLockState(ctx)
 	if err != nil {
-		t.Fatalf("isCurrentLockOwner failed: %v", err)
+		t.Fatalf("Failed to sync lock state: %s", err)
 	}
-	if !isOwner {
+	if lock1.lockState.state != LockAcquired {
 		t.Fatalf("%s is not the owner of this lock.", lock1.Uuid)
 	}
 
@@ -117,7 +121,7 @@ func TestAquireLockAfterTimeout(t *testing.T) {
 	}
 
 	// Assert - release lock.
-	versions, err := utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-39")
+	versions, err := utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-102")
 	if err != nil {
 		t.Fatalf("GetAllObjectVersions failed: %v", err)
 	}
@@ -126,14 +130,14 @@ func TestAquireLockAfterTimeout(t *testing.T) {
 	}
 
 	// Act - acquire lock after timeout.
-	time.Sleep(time.Second * 5)
+	time.Sleep(time.Second * 6)
 	err = lock2.AcquireLock(ctx)
 	if err != nil {
-		t.Fatalf("AcquireLock succeded, it should not.")
+		t.Fatalf("AcquireLock succeded, it should not. %s", err)
 	}
 
 	// Assert - release lock.
-	versions, err = utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-39")
+	versions, err = utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-102")
 	if err != nil {
 		t.Fatalf("GetAllObjectVersions failed: %v", err)
 	}
@@ -148,19 +152,19 @@ func TestAtomicity_SeveralLockInstancesCompete(t *testing.T) {
 	// Arrange.
 	ctx := context.Background()
 	config := setupConfig(ctx)
-	lock1 := NewS3Lock(config, "testlock1", "versioningbucketcrossbreed", "key-39", 4)
+	lock1 := NewS3Lock(config, "testlock1", "versioningbucketcrossbreed", "key-103", 4)
 	competingLocks := []*S3Lock{
-		NewS3Lock(config, "testlock2", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock3", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock4", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock6", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock7", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock8", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock9", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock10", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock11", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock12", "versioningbucketcrossbreed", "key-39", 4),
-		NewS3Lock(config, "testlock13", "versioningbucketcrossbreed", "key-39", 4),
+		NewS3Lock(config, "testlock2", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock3", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock4", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock6", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock7", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock8", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock9", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock10", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock11", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock12", "versioningbucketcrossbreed", "key-103", 4),
+		NewS3Lock(config, "testlock13", "versioningbucketcrossbreed", "key-103", 4),
 	}
 
 	// Act - acquire lock.
@@ -169,19 +173,20 @@ func TestAtomicity_SeveralLockInstancesCompete(t *testing.T) {
 		t.Fatalf("Lock1 failed to acquire lock.")
 	}
 
-	// Create a channel to collect results
+	// Create a channel to collect results.
 	results := make(chan struct {
 		Name string
 		Err  error
 	}, len(competingLocks))
 
-	// Use WaitGroup to wait for all Goroutines to complete
+	// Use WaitGroup to wait for all Goroutines to complete.
 	var wg sync.WaitGroup
 
 	for _, lock := range competingLocks {
 		wg.Add(1)
 		go func(l *S3Lock) {
-			defer wg.Done() // Decrement counter when Goroutine completes
+			// Decrement counter when Goroutine completes.
+			defer wg.Done()
 
 			err := l.AcquireLockWithRetry(ctx, time.Second*6)
 			results <- struct {
@@ -191,13 +196,13 @@ func TestAtomicity_SeveralLockInstancesCompete(t *testing.T) {
 		}(lock)
 	}
 
-	// Close the results channel after all Goroutines have completed
+	// Close the results channel after all Goroutines have completed.
 	go func() {
 		wg.Wait()
 		close(results)
 	}()
 
-	// Collect and display results
+	// Collect and display results.
 	successCounter := 0
 	for result := range results {
 		if result.Err != nil {
@@ -208,46 +213,11 @@ func TestAtomicity_SeveralLockInstancesCompete(t *testing.T) {
 		}
 	}
 
+	// If more than one instance acquired the lock. Fail.
 	if successCounter != 1 {
 		t.Fatalf("More then one (%v) lock think it has aquired the lock.", successCounter)
 	}
 }
-
-//func TestAcquireLock(t *testing.T) {
-//	ctx := context.Background()
-//	config, err := config.LoadDefaultConfig(ctx)
-//	if err != nil {
-//		panic("Could not load config.")
-//	}
-//
-//	lock1 := s3lock.NewS3Lock(config, "testlock", "versioningbucketcrossbreed", "key-39", 15)
-//	//lock2 := s3lock.NewS3Lock(config, "testlock2", "versioningbucketcrossbreed", "key-33", 15)
-//	//err = lock1.AcquireLock(ctx)
-//	resp, _ := utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-39")
-//	id := resp[0].VersionId
-//	//t.Logf("Resp: %+v", resp)
-//	//err = lock2.AcquireLockWithRetry(ctx, time.Second*20)
-//	//t.Errorf("%s", err)
-//
-//	//_, err = utils.DeleteObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", []types.ObjectVersion{})
-//	_, found, err := utils.HeadObject(ctx, lock1.Client, "versioningbucketcrossbreed", "key-40", aws.String(*id))
-//	t.Errorf("Error: %s", err)
-//	t.Logf("Found: %+v", found)
-//}
-
-//func assertIsLockOwner(string owner) {
-//versions, err := utils.GetAllObjectVersions(ctx, lock1.Client, "versioningbucketcrossbreed", "key-39")
-//if err != nil {
-//	t.Fatalf("GetAllObjectVersions failed: %v", err)
-//}
-//resp, _, err := utils.GetObject(ctx, lock1.Client, "versioningbucketcrossbreed", "key-39", versions[0].VersionId)
-//if err != nil {
-//	t.Fatalf("GetAllObject failed: %v", err)
-//}
-//if resp.Metadata["lockownerr"] != lock1.Uuid {
-//	t.Fatalf("Lockowner does not match.")
-//}
-//}
 
 func setupConfig(ctx context.Context) aws.Config {
 	config, err := config.LoadDefaultConfig(ctx)
