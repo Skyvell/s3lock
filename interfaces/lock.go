9cc6f23032b72e08a52e0fdf323747be4c0150b7
Ted Skyvell <tskyvell@gmail.com>
2023-08-27 19:17:27 +0200

    Added new method to interface.

--- interfaces/lock.go
+++ interfaces/lock.go
@@ -9,4 +9,5 @@ type ILock interface {
 	AcquireLock(ctx context.Context) error
 	AcquireLockWithRetry(ctx context.Context, timeout time.Duration) error
 	ReleaseLock(ctx context.Context) error
+	RemoveLockIfOwner(ctx context.Context) error
 }
